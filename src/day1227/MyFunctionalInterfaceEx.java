8049c6bdf682398f0a63936de0cdb7bc18211819
olsi10 <wangbampang2035@gmail.com>
2022-12-27 15:49:14 +0900

    람다식 사용해보기

---
+++ src/day1227/MyFunctionalInterfaceEx.java
@@ -0,0 +1,26 @@
+package day1227;
+
+@FunctionalInterface
+public interface MyFunctionalInterface {
+    public void method();
+}
+
+public class MyFunctionalInterfaceEx {
+    public static void main(String[] args) {
+        MyFunctionalInterface f1;
+
+        f1 = () -> {
+            String str = "method call1";
+            System.out.println(str);
+        };
+        f1.method();
+
+        f1 = () -> {
+            System.out.println("method call2");
+        };
+        f1.method();
+
+        f1 = () -> System.out.println("method call3");
+        f1.method();
+    }
+}
